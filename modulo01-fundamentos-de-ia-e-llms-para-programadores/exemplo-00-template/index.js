89f40a44fc78f60dffb270762a87d501660277d1
Bryann Brandao <bryann.brandao@bernoulli.com.br>
2026-03-10 11:41:35 -0300

    [Aula.01.02.05] Testando primeiro modelo

--- modulo01-fundamentos-de-ia-e-llms-para-programadores/exemplo-00-template/index.js
+++ modulo01-fundamentos-de-ia-e-llms-para-programadores/exemplo-00-template/index.js
@@ -48,14 +48,25 @@ async function trainModel(inputXs, outputYs) {
             epochs: 100, // Número de vezes que o modelo verá todo o dataset
             shuffle: true, // Embaralha os dados a cada época para evitar padrões de ordem
             callbacks: {
-                onEpochEnd: (epoch, logs) => {
-                    console.log(`epoch ${epoch}: loss = ${logs.loss}`);
-                }
+                // onEpochEnd: (epoch, logs) => {
+                //     console.log(`epoch ${epoch}: loss = ${logs.loss}`);
+                // }
             }
         }
     );
 
     return model;
+}
+
+async function predict(model, pessoa) {
+    // Transforma o array js para o tensor (tfjs)
+    const tInput = tf.tensor2d(pessoa);
+
+    // Fazendo a previsão (output será um vetor de 3 pprobabilidades)
+    const pred = model.predict(tInput);
+    const predArray = await pred.array();
+
+    return predArray[0].map((prob, index) => ({ prob, index }));
 }
 
 // Exemplo de pessoas para treino (cada pessoa com idade, cor e localização)
@@ -94,9 +105,34 @@ const tensorLabels = [
 const inputXs = tf.tensor2d(tensorPessoasNormalizado)
 const outputYs = tf.tensor2d(tensorLabels)
 
-inputXs.print();
-outputYs.print();
+// inputXs.print();
+// outputYs.print();
 
 // Quanto mais dados melhor!
 // Assim o algoritmo entende melhor os padrões e consegue generalizar melhor para novos casos
-const models = trainModel(inputXs, outputYs);
+const model = await trainModel(inputXs, outputYs);
+
+const pessoa = { nome: 'Zé', cor: 'verde', idade: 28, localizacao: 'Curitiba' };
+
+// Normalizando a idade da nova pessoa usando o mesmo padrão do treino
+// Exemplo: idade_min = 25, idade_max = 40, então (28 - 25) / (40 - 25) = 0.2
+const pessoaTensorNormalizado = [
+    [
+        0.2, // idade normalizada
+        1,   // azul
+        0,   // vermelho
+        0,   // verde
+        1,   // São Paulo
+        0,   // Rio
+        0    // Curitiba
+
+    ]
+];
+
+const predictions = await predict(model, pessoaTensorNormalizado);
+const results = predictions
+    .sort((a, b) => b.prob - a.prob)
+    .map(p => `${labelsNomes[p.index]}: ${(p.prob * 100).toFixed(2)}%`)
+    .join('\n');
+
+console.log(results);
