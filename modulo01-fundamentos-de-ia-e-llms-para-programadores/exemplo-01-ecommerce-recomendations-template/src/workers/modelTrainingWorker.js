695e41418a3606b17eaf58d6c656b17d56776fcb
Bryann Brandao <bryann.brandao@bernoulli.com.br>
2026-03-12 11:09:04 -0300

    [Aula.01.03.03] Analisando e preparando dados para treinamento

--- modulo01-fundamentos-de-ia-e-llms-para-programadores/exemplo-01-ecommerce-recomendations-template/src/workers/modelTrainingWorker.js
+++ modulo01-fundamentos-de-ia-e-llms-para-programadores/exemplo-01-ecommerce-recomendations-template/src/workers/modelTrainingWorker.js
@@ -3,12 +3,121 @@ import { workerEvents } from '../events/constants.js';
 
 console.log('Model training worker initialized');
 let _globalCtx = {};
+const WEIGHTS = {
+    category: 0.4,
+    color: 0.3,
+    price: 0.2,
+    age: 0.1,
+};
 
+// Normalização dos valores para ficar entre 0 e 1
+const normalize = (value, min, max) => (value - min) / ((max - min) || 1);
+
+function makeContext(catalog, users) {
+    const ages = users.map(u => u.age);
+    const prices = catalog.map(p => p.price);
+    const minAge = Math.min(...ages);
+    const maxAge = Math.max(...ages);
+    const minPrice = Math.min(...prices);
+    const maxPrice = Math.max(...prices);
+    const colors = [...new Set(catalog.map(p => p.color))];
+    const categories = [...new Set(catalog.map(p => p.category))];
+    const colorsIndex = Object.fromEntries(
+        colors.map((color, index) => [color, index])
+    );
+    const categoriesIndex = Object.fromEntries(
+        categories.map((category, index) => [category, index])
+    );
+
+    // Computader média de idade dos compradores por produto
+    // (Ajuda a personalizar)
+    const midAge = (minAge + maxAge) / 2;
+    const ageSums = {};
+    const ageCounts = {};
+
+    users.forEach(user => {
+        user.purchases.forEach(p => {
+            ageSums[p.name] = (ageSums[p.name] || 0) + user.age;
+            ageCounts[p.name] = (ageCounts[p.name] || 0) + 1;
+        })
+    });
+    const productAvgAgeNorm = Object.fromEntries(
+        catalog.map(p => {
+            const avg = ageCounts[p.name]
+                ? ageSums[p.name] / ageCounts[p.name]
+                : midAge
+
+                return [p.name, normalize(avg, minAge, maxAge)]
+        })
+    );
+
+    return {
+        catalog,
+        users,
+        colorsIndex,
+        categoriesIndex,
+        minAge,
+        maxAge,
+        minPrice,
+        maxPrice,
+        numCategories: categories.length,
+        numColors: colors.length,
+        productAvgAgeNorm,
+        // Price + age + colors + categories
+        dimensions: 2 + categories.length + colors.length,
+    }
+}
+
+const oneHotWeighted = (index, length, weight) =>
+    tf.oneHot(index, length).cast('float32').mul(weight);
+
+function encodeProduct(product, context) {
+    const price = tf.tensor1d([
+        // normalizando para ficar de 0 a 1 e aplica peso
+        normalize(
+            product.price,
+            context.minPrice,
+            context.maxPrice
+        ) * WEIGHTS.price
+    ]);
+    const age = tf.tensor1d([
+        (
+            context.productAvgAgeNorm[product.name] || 0.5
+        ) * WEIGHTS.age
+    ]);
+    const category = oneHotWeighted(
+        context.categoriesIndex[product.category],
+        context.numCategories,
+        WEIGHTS.category
+    );
+    const color = oneHotWeighted(
+        context.colorsIndex[product.color],
+        context.numCategories,
+        WEIGHTS.color
+    );
+
+    return tf.concat1d([price, age, category, color]);
+}
 
 async function trainModel({ users }) {
     console.log('Training model with users:', users)
 
     postMessage({ type: workerEvents.progressUpdate, progress: { progress: 50 } });
+    const catalog = await fetch('/data/products.json').then(res => res.json());
+    
+    const context = makeContext(catalog, users);
+    context.productVectors = catalog.map(product => {
+        return {
+            name: product.name,
+            meta: { ...product },
+            vector: encodeProduct(product, context).dataSync(),
+        }
+    });
+
+    debugger
+
+    _globalCtx = context;
+
     postMessage({
         type: workerEvents.trainingLog,
         epoch: 1,
